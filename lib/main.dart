d436f869f3ad30669d905e4853d52e2e33ab38de
hebamsamy <hebasamy655@gmail.com>
2025-02-12 09:43:56 +0200

    day one

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:app/welcomescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: WelcomeScreen(),
+    // routes: {
+
+    // },
+  ));
+}
---
+++ lib/welcomescreen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Our App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // crossAxisAlignment: ,
+          children: [
+            // Image.network("https://th.bing.com/th/id/OIP.1d6tBbNiJTFQNEK_k0sSjQHaFj?rs=1&pid=ImgDetMain"),
+            Image.asset("images/img.jpeg"),
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                  fontSize: 50, color: const Color.fromARGB(255, 25, 127, 178)),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.heart_broken,color: Colors.red,size: 40,),
+                Text("Join US ")
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
